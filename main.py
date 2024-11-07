e28071e943c124bd4a717640d5fb7e2891a14f70
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-07 12:25:33 -0500

    Solution exercise 1

---
+++ main.py
@@ -0,0 +1,8 @@
+# Escribe un programa que verifique si un número es par o impar utilizando if
+
+number = float(input('Give me a number: '))
+
+if number % 2 == 0:
+    print('This is a pair number!')
+else:
+    print('This is an unpair number!')
