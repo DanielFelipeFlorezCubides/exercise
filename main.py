50c4e22acaed6f0d53badaf4959c8cde3ba7d0e2
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-07 13:08:03 -0500

    Exercise 3 solution

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-# Escribe un programa que determine si una nota numérica es "Aprobado" o "Reprobado" usando if.
+# Utiliza match para implementar una calculadora simple.
 
-grade = float(input('Please gimme the grade you scored (0/10): '))
+def calculator(number_1, number_2, operation):
+    match operation:
 
-if grade < 6.5:
-    print('Failed')
-else:
-    print('Approved')
+        case '+':
+            sum = number_1 + number_2
+            print(sum)
+        case '-':
+           minus = number_1 - number_2
+           print(minus)
+        case 'x':
+            times = number_1 * number_2
+            print(times)
+        case '/':
+            division = number_1 / number_2
+            print(division)
+        case _:
+            print('''Operation didn't recognize''')
+
+operation = input('Please tell me which operation you wanna execute( + , - , x , /): ')
+number_1 = int(input('Please gimme a number: '))
+number_2 = int(input('Please gimme another number: '))
+    
+calculator(number_1, number_2,operation)
