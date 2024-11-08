7b8a9eb400bd2a6692ae2f2b64354af8c38ee401
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-07 22:48:21 -0500

    Exercise 6 solution

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-# Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la semana usando match.
-# Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes, 7 = Domingo).
+# Escribe un programa que implemente un juego de adivinanza de números.
+# El programa genera un número aleatorio entre 1 y 10. El usuario debe adivinar el número, 
+# y el programa indica si el número ingresado es mayor, menor o igual al número generado.
+import random
 
-def semana(number):
-    match number:
-        case 1:
-            monday = 'Today is Monday!'
-            print(monday)
-        case 2:
-            tuesday = 'Today is Tuesday!'
-            print(tuesday)
-        case 3:
-            wednesday = 'Today is Wednesday!'
-            print(wednesday)
-        case 4:
-            thursday = 'Today is Thursday!'
-            print(thursday)
-        case 5:
-            friday = 'Today is Friday!'
-            print(friday)
-        case 6:
-            saturday = 'Today is Saturday!'
-            print(saturday)
-        case 7:
-            sunday = 'Today is Sunday!'
-            print(sunday)
+def guessing_game():
+    rNumber = int(random.randint(1,10))
+    print('I generated a random number between 1 to 10, try to guess wich one is it!')
 
-number = int(input('Please type a number from 1 to 7: '))
-semana(number)
+    guess = False
+
+    while not guess:
+        n = int(input('Type your guess: '))
+        if n < rNumber:
+            print('Your guess is less than the choosen one!')
+        elif n > rNumber:
+            print('Your guess is higher than the choosen one!')
+        else:
+            print(f'Congrats, you have guessed the number!')
+            guess = True
+
+guessing_game()
