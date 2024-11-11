2d6b778d191710ee75bd2292e77bbf7a07131184
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-11 17:53:25 -0500

    Exercise 9 solution

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# Escribe un programa que determine si un año es bisiesto o no.
-# Solicita al usuario que ingrese un año y determina si es bisiesto 
-# (divisible entre 4, pero no entre 100, salvo que sea divisible entre 400).
+# Escribe un programa que clasifique a una persona en función de su edad.
+# Solicita la edad de la persona e indica si es niño (0-12 años), 
+# adolescente (13-17 años), adulto (18-64 años) o anciano (65 años o más).
 
-year = float(input('Please type the year number: '))
-leap = False
+age = int(input('Plese type your age: '))
 
-if year % 400 == 0:
-    leap = True
-elif year % 100 == 0:
-    leap = False
-elif year % 4 == 0:
-    leap = True
-
-if leap:
-    print('This is a leap year')
+if age <= 12:
+    print('''You're a kid''')
+elif age > 12 and age <= 17:
+    print('''You're a teenager''')
+elif age > 17 and age <= 64:
+    print('''You're an adult''')
+elif age > 64 and age <= 120:
+    print('''You're a senior''')
 else:
-    print('This is not a leap year')
+    print('Error, a human cannot live more than 120 years')
