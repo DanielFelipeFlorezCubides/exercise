f7ff3368c55341fb41cabcfff8af71b3d5b7b06f
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-11 20:21:39 -0500

    Exercise 17 solution

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# Escribe un programa que calcule el tiempo que tarda en llegar un automóvil a su destino.
-# Solicita al usuario la distancia a recorrer (en km) y la velocidad promedio del automóvil (en km/h). 
-# Calcula el tiempo de viaje en horas y minutos. Si la velocidad es mayor a 120 km/h, muestra un mensaje de advertencia.
+# Escribe un programa que calcule la calificación final de un estudiante basándose en su calificación y si ha hecho tareas adicionales.
+# Solicita la calificación del estudiante y pregunta si hizo tareas adicionales. 
+# Si la respuesta es "sí", añade un 5% extra a la calificación, pero si la calificación supera 100, ajústala a 100. 
+# Si la respuesta es "no", simplemente muestra la calificación original.
 
-distance = float(input("Type the distance(km): "))
-speed = float(input("Type the speed(km/h): "))
 
-if distance > 0 and speed > 0:
-    if speed > 120:
-        print("You are going too fast")
-    
-    time = distance / speed 
-    if time < 1:
-        time = time * 60
-        print("The travel time will be:",round(time), "minutes")
-    else:
-        integer = int(time)
-        minutes = (time - integer)*60
-        print("The travel time will be: ", integer, "hours and", round(minutes), "minutes")
+def bonificacion(grade,aditional):
+    if aditional == "Y" and grade <100 and grade >= 0: 
+        grade = (grade *0.05) + grade
+        if grade > 100:
+            grade = 100
+    print("The final grade is:",grade)
+
+grade = float(input("Type your grade: "))
+aditional = input("Does the student got bonifications? (Y/N): ").upper()
+bonificacion(grade,aditional)
