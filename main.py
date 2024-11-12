bb21ba861f1f7d4c2f3fdf9700bc6aed6c56f175
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-11 20:52:58 -0500

    Exercise 21 solution

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-# Escribe un programa que convierta una calificación numérica en una letra de acuerdo 
-# a un sistema de calificación específico, usando match.
-# Solicita una calificación numérica (0-100) y convierte esa calificación a una letra usando el siguiente esquema:
+# Escribe un programa que calcule el costo de estacionamiento basado en el número de horas, con tarifas progresivas.
+# El costo de estacionamiento se calcula de la siguiente manera:
 
-# A: 90-100
-# B: 80-89
-# C: 70-79
-# D: 60-69
-# F: 0-59
+# Primera hora: $5
+# Segunda a cuarta hora: $4 por hora
+# Más de cuatro horas: $3 por cada hora adicional
+# Solicita al usuario el número de horas de estacionamiento y calcula el costo total.
 
-def grade_classification(grade):
-    match grade:
-        case g if 90 <= g <= 100:
-            return "A"
-        case g if 80 <= g <= 89:
-            return "B"
-        case g if 70 <= g <= 79:
-            return "C"
-        case g if 60 <= g <= 69:
-            return "D"
-        case g if 0 <= g <= 59:
-            return "F"
-        case _:
-            print('That number is out of range')
-
-grade = float(input('Please type your grade: '))
-classification = grade_classification(grade)
-print(f'{classification}')
+hours = int(input("Type the parking hours: "))
+total = 0
+additionalHours = hours - 4 
+if hours == 1:
+    total += 5
+elif 2 <= hours <= 4:
+    total += 5 + ((hours - 1) * 4)
+elif hours > 4:
+    total += 17 + (additionalHours * 3)
+        
+print(f"The total ammount to pay will be: ${total}")
