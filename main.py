4f86edcfd59e4bdd20de527963a112626d378016
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-11 20:38:05 -0500

    Exercise 18 solution

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# Escribe un programa que calcule la calificación final de un estudiante basándose en su calificación y si ha hecho tareas adicionales.
-# Solicita la calificación del estudiante y pregunta si hizo tareas adicionales. 
-# Si la respuesta es "sí", añade un 5% extra a la calificación, pero si la calificación supera 100, ajústala a 100. 
-# Si la respuesta es "no", simplemente muestra la calificación original.
+# Escribe un programa que calcule el número de créditos totales de un estudiante 
+# en base a las materias cursadas y el puntaje obtenido en cada una.
+# Solicita al usuario ingresar el número de materias que ha cursado. 
+# Para cada materia, solicita el puntaje y determina si ha aprobado o no (>= 60). 
+# Luego, calcula el número total de créditos del estudiante (cada materia aprobada otorga 3 créditos).
 
+n = int(input('Type the subjects number you were coursing: '))
+credits = 0
 
-def bonificacion(grade,aditional):
-    if aditional == "Y" and grade <100 and grade >= 0: 
-        grade = (grade *0.05) + grade
-        if grade > 100:
-            grade = 100
-    print("The final grade is:",grade)
+for i in range(n):
+    grade = int(input(f'Type your grade on {i+1} subject (0 - 100): '))
+    
+    if grade >= 60:
+        credits = credits + 3
 
-grade = float(input("Type your grade: "))
-aditional = input("Does the student got bonifications? (Y/N): ").upper()
-bonificacion(grade,aditional)
+print(f'The credits number you reached were: {credits}')
