c992bffa40b5b1d0a9cea2e6da1e554a9b690653
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-11-11 20:14:34 -0500

    Exercise 16 solution

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-# Escribe un programa que calcule el salario neto de un empleado después de aplicar impuestos.
-# Solicita al usuario su salario bruto y su país de residencia. 
-# Calcula el salario neto aplicando impuestos: el 20% para residentes de "País A", 
-# el 15% para "País B" y el 10% para "País C". Si el país no está en la lista, aplica un 25% de impuestos.
+# Escribe un programa que calcule el tiempo que tarda en llegar un automóvil a su destino.
+# Solicita al usuario la distancia a recorrer (en km) y la velocidad promedio del automóvil (en km/h). 
+# Calcula el tiempo de viaje en horas y minutos. Si la velocidad es mayor a 120 km/h, muestra un mensaje de advertencia.
 
-wage = float(input("Type your wage: "))
-country = input("Type your country (A,B,C) countries that are not listed will be deducted with 25%: ")
+distance = float(input("Type the distance(km): "))
+speed = float(input("Type the speed(km/h): "))
 
-if country.upper() == "A":
-    print("Taxes payment of 20'%' has made")
-    discount = wage * 0.20
-    new_wage = wage - discount
-    print(f"Your income is: {new_wage}")
-elif country.upper() == "B":
-    print("Taxes payment of 15'%' has made")
-    discount = wage * 0.15
-    new_wage = wage - discount
-    print(f"Your income is: {new_wage}")
-elif country.upper() == "C":
-    print("Taxes payment of 10'%' has made")
-    discount = wage * 0.10
-    new_wage = wage - discount
-    print(f"Your income is: {new_wage}")
-else:
-    print("Taxes payment of 25'%' has made")
-    discount = wage * 0.25
-    new_wage = wage - discount
-    print(f"Your income is: {new_wage}")
+if distance > 0 and speed > 0:
+    if speed > 120:
+        print("You are going too fast")
+    
+    time = distance / speed 
+    if time < 1:
+        time = time * 60
+        print("The travel time will be:",round(time), "minutes")
+    else:
+        integer = int(time)
+        minutes = (time - integer)*60
+        print("The travel time will be: ", integer, "hours and", round(minutes), "minutes")
